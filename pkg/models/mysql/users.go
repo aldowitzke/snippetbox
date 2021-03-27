ba1fc2af5576cc4e6039a8c0bbf3c109463ea4bf
aldowitzke <aldo_mwa@hotmail.com>
2021-03-27 00:25:31 -0300

    Add user login and authentication

--- pkg/models/mysql/users.go
+++ pkg/models/mysql/users.go
@@ -3,14 +3,41 @@ package mysql
 import (
 	"database/sql"
 	"errors"
+	"strings"
+
 	"github.com/aldowitzke/snippetbox/pkg/models"
 	"github.com/go-sql-driver/mysql"
 	"golang.org/x/crypto/bcrypt"
-	"strings"
 )
 
 type UserModel struct {
 	DB *sql.DB
+}
+
+func (m *UserModel) Authenticate(email, password string) (int, error) {
+	var id int
+	var hashedPassword []byte
+	stmt := "SELECT id, hashed_password FROM users WHERE email = ? and ACTIVE = TRUE"
+	row := m.DB.QueryRow(stmt, email)
+	err := row.Scan(&id, &hashedPassword)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, models.ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, models.ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	return id, nil
 }
 
 func (m *UserModel) Insert(name, email, password string) error {
@@ -34,10 +61,6 @@ func (m *UserModel) Insert(name, email, password string) error {
 	}
 
 	return nil
-}
-
-func (m *UserModel) Authenticate(email, password string) (int, error) {
-	return 0, nil
 }
 
 func (m *UserModel) Get(id int) (*models.User, error) {
