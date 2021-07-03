c32ade6399d3b65640d6d99f25682a0fa07a3379
aldowitzke <aldo_mwa@hotmail.com>
2021-07-03 17:18:39 -0300

    Create user and snippet mocks for testing

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,17 +13,26 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/aldowitzke/snippetbox/pkg/models"
 	"github.com/aldowitzke/snippetbox/pkg/models/mysql"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
-	errorLog      *log.Logger
-	infoLog       *log.Logger
-	session       *sessions.Session
-	snippets      *mysql.SnippetModel
+	errorLog *log.Logger
+	infoLog  *log.Logger
+	session  *sessions.Session
+	snippets interface {
+		Insert(string, string, string) (int, error)
+		Get(int) (*models.Snippet, error)
+		Latest() ([]*models.Snippet, error)
+	}
 	templateCache map[string]*template.Template
-	users         *mysql.UserModel
+	users         interface {
+		Insert(string, string, string) error
+		Authenticate(string, string) (int, error)
+		Get(int) (*models.User, error)
+	}
 }
 
 type contextKey string
